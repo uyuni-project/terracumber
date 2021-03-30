2436549933f908034d8b73ad378c3c638baf1540
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2021-03-30 14:53:13 +0200

    Overwrite exising configurations with environment variables
    This is useful so we can change configuration "on the fly" when running
    tests.
    
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.de>

--- terracumber/test/test_utils.py
+++ terracumber/test/test_utils.py
@@ -15,6 +15,25 @@ class TestUtils(unittest.TestCase):
         dict_y = { }
         self.assertDictEqual(utils.merge_two_dicts(dict_x, dict_y), { })
 
+    def test_overwrite_dict(self):
+        dict_x = { 'a': 1, 'b': 2 }
+        dict_y = { 'b': 22, 'c': 3 }
+        dict_z = utils.overwrite_dict(dict_x, dict_y)
+        self.assertDictEqual(dict_z, { 'a': 1, 'b': 22 })
+        self.assertDictEqual(dict_x, { 'a': 1, 'b': 2 })
+        self.assertDictEqual(dict_y, { 'b': 22, 'c': 3 })
+
+        dict_x = { 'a': 1, 'b': 2 }
+        dict_y = { }
+        self.assertDictEqual(utils.overwrite_dict(dict_x, dict_y), { 'a': 1, 'b': 2 })
+
+        dict_x = { }
+        dict_y = { 'a': 1, 'b': 2 }
+        self.assertDictEqual(utils.overwrite_dict(dict_x, dict_y), { })
+
+        dict_x = { }
+        dict_y = { }
+        self.assertDictEqual(utils.overwrite_dict(dict_x, dict_y), { })
 
 if __name__ == '__main__':
     unittest.main()
