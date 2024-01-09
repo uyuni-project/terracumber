1d8943398bbb1b1aa86339bdeb094a7603e5c1e3
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2024-01-09 15:35:30 +0000

    Add missing testing files and prevent test issues

--- test/test_git.py
+++ test/test_git.py
@@ -6,7 +6,7 @@ from unittest.mock import patch
 
 class TestGit(unittest.TestCase):
     def setUp(self):
-        self.repo_url = 'https://gitlab.suse.de/juliogonzalezgil/terracumber.git'
+        self.repo_url = 'https://github.com/uyuni-project/terracumber.git'
         self.ref = 'master'
         self.folder = '/tmp/folder'
         self.username = 'myuser'
@@ -48,12 +48,12 @@ class TestGit(unittest.TestCase):
     @patch('terracumber.git.Git.checkout')
     def test_is_remote(self, mock_checkout, mock_remote, mock_repository):
         mock_remote.name = 'origin'
-        mock_remote.url = 'https://gitlab.suse.de/juliogonzalezgil/terracumber.git'
+        mock_remote.url = 'https://github.com/uyuni-project/terracumber.git'
         mock_repository.return_value.remotes = [mock_remote]
         self.repo = git.Git(self.repo_url, 'mytag', self.folder, {'user': self.username, 'password': self.password},
                             self.auto)
         self.assertTrue(self.repo.is_remote())
-        self.repo = git.Git('https://gitlab.suse.de/juliogonzalezgil/terracumber2.git', 'mytag', self.folder,
+        self.repo = git.Git('https://github.com/uyuni-project/terracumber2.git', 'mytag', self.folder,
                             {'user': self.username, 'password': self.password}, self.auto)
         self.assertFalse(self.repo.is_remote())
 
@@ -68,14 +68,14 @@ class TestGit(unittest.TestCase):
 
     @patch('terracumber.git.pygit2.Repository')
     def test_create_remote_from_url(self, mock_pygit2):
-        self.repo = git.Git('https://gitlab.suse.de/juliogonzalezgil/terracumber2.git', 'mytag', self.folder,
+        self.repo = git.Git('https://github.com/uyuni-project/terracumber2.git', 'mytag', self.folder,
                             {'user': self.username, 'password': self.password}, self.auto)
-        self.assertEqual(self.repo.create_remote_from_url(), 'https---gitlab-suse-de-juliogonzalezgil-terracumber2-git')
+        self.assertEqual(self.repo.create_remote_from_url(), 'https---github-com-uyuni-project-terracumber2-git')
 
     @patch('terracumber.git.os.path.isdir', return_value=True)
     @patch('terracumber.git.pygit2.Repository')
-    @patch('terracumber.git.Git.refresh_local_repo', return_value=('https---gitlab-suse-de-juliogonzalezgil-terracumber-git',
-                                                       'https://gitlab.suse.de/juliogonzalezgil/terracumber.git'))
+    @patch('terracumber.git.Git.refresh_local_repo', return_value=('https---github-com-uyuni-project-terracumber-git',
+                                                       'https://github.com/uyuni-project/terracumber.git'))
     def test_refresh_local_repo(self, mock_refres_local_repo, mock_repository, mock_is_dir):
         mock_repository.return_value.listall_references.return_value = ['ref/heads/master', 'refs/tags/mytag']
         # Simulate invalid references
@@ -83,24 +83,24 @@ class TestGit(unittest.TestCase):
         # Tag exists locally but not on the remote
         mock_repository.return_value.lookup_reference.side_effect = KeyError()
         with self.assertRaises(Exception) as e:
-            self.repo = git.Git('https://gitlab.suse.de/juliogonzalezgil/terracumber.git', 'mytag', self.folder,
+            self.repo = git.Git('https://github.com/uyuni-project/terracumber.git', 'mytag', self.folder,
                                 {'user': self.username, 'password': self.password}, self.auto)
         self.assertEqual(str(e.exception),
-                         "Could not find reference refs/tags/mytag (remote URL https://gitlab.suse.de/juliogonzalezgil/terracumber.git)")
+                         "Could not find reference refs/tags/mytag (remote URL https://github.com/uyuni-project/terracumber.git)")
         # Branch exists locally but not on the remote
         with self.assertRaises(Exception) as e:
-            self.repo = git.Git('https://gitlab.suse.de/juliogonzalezgil/terracumber.git', 'master', self.folder,
+            self.repo = git.Git('https://github.com/uyuni-project/terracumber.git', 'master', self.folder,
                                 {'user': self.username, 'password': self.password}, self.auto)
         self.assertEqual(str(e.exception),
-                         "Could not find reference refs/remotes/https---gitlab-suse-de-juliogonzalezgil-terracumber-git/master (remote URL https://gitlab.suse.de/juliogonzalezgil/terracumber.git)")
+                         "Could not find reference refs/remotes/https---github-com-uyuni-project-terracumber-git/master (remote URL https://github.com/uyuni-project/terracumber.git)")
         # Simulate valid references
         mock_repository.return_value.lookup_reference.side_effect = None
         # Tag exists locally and on the remote
-        self.repo = git.Git('https://gitlab.suse.de/juliogonzalezgil/terracumber.git', 'mytag', self.folder,
+        self.repo = git.Git('https://github.com/uyuni-project/terracumber.git', 'mytag', self.folder,
                             {'user': self.username, 'password': self.password}, self.auto)
         self.assertFalse(self.repo.reset_hard)
         # Branch exists locally and on the remote
-        self.repo = git.Git('https://gitlab.suse.de/juliogonzalezgil/terracumber.git', 'mybranch', self.folder,
+        self.repo = git.Git('https://github.com/uyuni-project/terracumber.git', 'mybranch', self.folder,
                             {'user': self.username, 'password': self.password}, self.auto)
         self.assertTrue(self.repo.reset_hard)
 
