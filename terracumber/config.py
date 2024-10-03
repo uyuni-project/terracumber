c2ba0d26235c2119270375585581658c3ab5afa4
Maxime Noel <maxime.noel@suse.com>
2024-10-03 14:17:35 +1300

    Upgrade hcl2

--- terracumber/config.py
+++ terracumber/config.py
@@ -1,13 +1,12 @@
 """Manage HCL files as configuration files"""
-import hcl
-
+import hcl2
 
 def read_config(path):
     """Return a dictionary with all the variables from a HCL file"""
     config = {}
     with open(path, 'r') as cfg:
-        hcl_file = hcl.load(cfg)
-        if not 'variable' in hcl_file.keys():
+        hcl_file = hcl2.load(cfg)  # Use hcl2 for better parsing
+        if 'variable' not in hcl_file.keys():
             return config
         for key, value in hcl_file['variable'].items():
             try:
