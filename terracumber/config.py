8304a8faf6e0557114f8ade2f44fcca7bfb137f9
Maxime Noel <maxime.noel@suse.com>
2025-11-20 14:12:33 +1300

    Variables can be split between tf variables and tf file

--- terracumber/config.py
+++ terracumber/config.py
@@ -1,12 +1,12 @@
 """Manage HCL files as configuration files"""
-import hcl
+import hcl2
 
 
 def read_config(path):
     """Return a dictionary with all the variables from a HCL file"""
     config = {}
     with open(path, 'r') as cfg:
-        hcl_file = hcl.load(cfg)
+        hcl_file = hcl2.load(cfg)
         if not 'variable' in hcl_file.keys():
             return config
         for key, value in hcl_file['variable'].items():
